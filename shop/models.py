e0c0ecde28634ffb416f90e5f67e9ef08ce94720
Janna-git <zhannat.shayakunova@mail.ru>
2023-04-12 09:51:52 +0600

    hw2

---
+++ shop/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+
+
+    def __str__(self):
+        return self.name
+
+class Purchase(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    data_purchase = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        verbose_name = 'Покупка'
+        verbose_name_plural = 'Покупки'
+
+
+
+    def __str__(self):
+        return f'{self.name} {self.age} {self.name}'
---
+++ shop/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+
+from .models import Item, Purchase
+
+def greeting(request):
+    return HttpResponse('Добро пожаловать в наш магазин')
+
+
+def list_item(request):
+    items = Item.objects.all()
+    context = {
+        'items': items,
+    }
+    return render(request, 'list_item.html', context)
+
+def detail_item(request, item_id):
+    item = Item.objects.get(name=item_id)
+    context = {
+        'item': item
+    }
+    return render(request, 'detail_item.html', context)
